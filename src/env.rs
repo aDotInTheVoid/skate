6dc3d245c00029778a7ab8e08caff81a201b6539
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2021-04-28 21:01:26 +0100

    Scope vec

--- src/env.rs
+++ src/env.rs
@@ -4,22 +4,44 @@ use crate::value::Value;
 
 #[derive(Debug, Default)]
 pub struct Scope<'a> {
-    vars: HashMap<&'a str, Value>,
+    vars: Vec<HashMap<&'a str, Value>>,
 }
 
 impl<'a> Scope<'a> {
     pub fn declare(&mut self, name: &'a str, v: Value) {
         // TODO: Handle collisions
-        self.vars.insert(name, v);
+        // self.vars.insert(name, v);
+        let last = match self.vars.last_mut() {
+            Some(x) => x,
+            None => {
+                self.vars.push(HashMap::new());
+                self.vars.last_mut().unwrap()
+            }
+        };
+        last.insert(name, v);
     }
 
     pub fn lookup(&mut self, name: &str) -> Option<Value> {
-        self.vars.get(name).map(Clone::clone)
+        // self.vars.get(name).map(Clone::clone)
+        let mut ret = None;
+        for i in self.vars.iter().rev() {
+            if let Some(var) = i.get(name) {
+                ret = Some(var.clone());
+                break;
+            }
+        }
+        ret
     }
 
     pub fn in_scope(&self) -> Vec<&'a str> {
-        let mut keys: Vec<&str> = self.vars.keys().copied().collect();
-        keys.sort_unstable();
-        keys
+        self.vars
+            .iter()
+            .rev()
+            .flat_map(|x| {
+                let mut keys: Vec<&str> = x.keys().copied().collect();
+                keys.sort_unstable();
+                keys
+            })
+            .collect()
     }
 }
