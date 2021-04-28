e33fc4ed2e7481ee1afca9af24a8ddd747868996
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2021-04-28 10:07:04 +0100

    Trailing newlines, for the last time
    Script it and be done

--- fmt.sh
+++ fmt.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 sd -s "#[cfg_attr(rustfmt, rustfmt_skip)]" "" src/grammar.rs
 cargo fmt
+
+# https://unix.stackexchange.com/a/161853
+git ls-files -z | while IFS= read -rd '' f; do tail -c1 < "$f" | read -r _ || echo >> "$f"; done
+
