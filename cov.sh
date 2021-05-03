a9ff37cbb75c727bb7f40cca6808390203fe8ea6
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2021-05-03 21:22:35 +0100

    More tests

--- cov.sh
+++ cov.sh
@@ -8,7 +8,7 @@ RUSTFLAGS="-Zinstrument-coverage" cargo +nightly build
 llvm-profdata merge -sparse ./cov/data/profile.*.profraw -o cov/out.profdata
 
 llvm-cov show   -Xdemangler=rustfilt cov/target/debug/skate -instr-profile  cov/out.profdata -show-line-counts-or-regions -show-instantiations -format=html -ignore-filename-regex="(.cargo)|(.rustup)" -line-coverage-lt=99 > cov/cov.html
-llvm-cov report -Xdemangler=rustfilt cov/target/debug/skate -instr-profile  cov/out.profdata  -ignore-filename-regex="(.cargo)|(.rustup)" -line-coverage-lt=99 > cov.txt
+# llvm-cov report -Xdemangler=rustfilt cov/target/debug/skate -instr-profile  cov/out.profdata  -ignore-filename-regex="(.cargo)|(.rustup)" -line-coverage-lt=99 > cov.txt
 
 
 rm default.profraw
