8868f6a78f6612df689484e1dd949e1b274356bd
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2021-05-14 19:38:58 +0100

    Dont get tarbombed

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,8 +3,9 @@ set -euxo pipefail
 curl -OL https://github.com/casey/just/releases/download/v0.9.2/just-v0.9.2-x86_64-unknown-linux-musl.tar.gz
 curl -OL https://github.com/rustwasm/wasm-pack/releases/download/v0.9.1/wasm-pack-v0.9.1-x86_64-unknown-linux-musl.tar.gz
 tar xf wasm-pack-v0.9.1-x86_64-unknown-linux-musl.tar.gz
-tar xf just-v0.9.2-x86_64-unknown-linux-musl.tar.gz
+mkdir just
+tar xf just-v0.9.2-x86_64-unknown-linux-musl.tar.gz -C just
 mkdir bin
 mv wasm-pack-v0.9.1-x86_64-unknown-linux-musl/wasm-pack bin
-mv just bin
+mv just/just bin
 echo "$(pwd)/bin" >> $GITHUB_PATH
