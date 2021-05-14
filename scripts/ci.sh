e4291cb1ceeefc9c9237ba3c57e52ba6236c9d34
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2021-05-14 19:04:18 +0100

    YAML Keys

---
+++ scripts/ci.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euxo pipefail
+curl -OL https://github.com/casey/just/releases/download/v0.9.2/just-v0.9.2-x86_64-unknown-linux-musl.tar.gz
+curl -OL https://github.com/rustwasm/wasm-pack/releases/download/v0.9.1/wasm-pack-v0.9.1-x86_64-unknown-linux-musl.tar.gz
+tar xf wasm-pack-v0.9.1-x86_64-unknown-linux-musl.tar.gz
+tar xf just-v0.9.2-x86_64-unknown-linux-musl.tar.gz
+mkdir bin
+mv wasm-pack-v0.9.1-x86_64-unknown-linux-musl/wasm-pack bin
+mv just bin
+PATH="$(pwd)/bin:$PATH"
