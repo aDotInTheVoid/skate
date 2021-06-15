af15c085bb949e284ef5e46d9811ba873b5b6d08
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2021-06-15 15:38:13 +0100

    Not map field access test

--- scripts/cov.sh
+++ scripts/cov.sh
@@ -7,8 +7,8 @@ RUSTFLAGS="-Zinstrument-coverage" cargo +nightly build
 ./scripts/test.py --coverage
 llvm-profdata merge -sparse ./cov/data/profile.*.profraw -o cov/out.profdata
 
-# llvm-cov show   -Xdemangler=rustfilt cov/target/debug/skate -instr-profile  cov/out.profdata -show-line-counts-or-regions -format=html  -ignore-filename-regex="(.cargo)|(.rustup)|(grammar.rs)" -show-instantiations=false -line-coverage-lt=99 > cov/cov.html
-llvm-cov show   -Xdemangler=rustfilt cov/target/debug/skate -instr-profile  cov/out.profdata -show-line-counts-or-regions  -format=html -ignore-filename-regex="(.cargo)|(.rustup)|(grammar.rs)" -show-instantiations=false > cov/cov.html
+llvm-cov show   -Xdemangler=rustfilt cov/target/debug/skate -instr-profile  cov/out.profdata -show-line-counts-or-regions -format=html  -ignore-filename-regex="(.cargo)|(.rustup)|(grammar.rs)" -show-instantiations=false -line-coverage-lt=99 > cov/cov.html
+# llvm-cov show   -Xdemangler=rustfilt cov/target/debug/skate -instr-profile  cov/out.profdata -show-line-counts-or-regions  -format=html -ignore-filename-regex="(.cargo)|(.rustup)|(grammar.rs)" -show-instantiations=false > cov/cov.html
 
 if [[ -r default.profraw ]]
 then
